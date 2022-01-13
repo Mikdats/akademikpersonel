74a31f2c30f8d61d393cb2774423bf534d286903
Mikdats <96742937+Mikdats@users.noreply.github.com>
2022-01-13 20:21:53 +0300

    Add files via upload

---
+++ akademik_personel/akademik_personel/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akademik_personel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            akademikpersonel personel = new akademikpersonel();
+            personel.personelad = "İlber";
+            personel.personelsoyad = "Ortaylı";
+            personel.personelunvan = "PROF.DR";
+            personel.personelmail = "İlbergmail.com";
+
+            Console.WriteLine("Personelin adı: " + personel.personelad);
+            Console.WriteLine("Personelin soyadı: " + personel.personelsoyad);
+            Console.WriteLine("Personelin unvan: " + personel.personelunvan);
+            Console.WriteLine("Personelin mail: " + personel.personelmail);
+
+            Console.ReadLine();
+
+        }
+    }
+}
